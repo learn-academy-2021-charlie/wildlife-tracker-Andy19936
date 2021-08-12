b8f14cbd43c6fb14d32426c5063bf1eb511541e8
Andy19936 <andy19936@gmail.com>
2021-08-12 16:16:44 -0700

    validations added rspec for Animal model completed

--- spec/models/animal_spec.rb
+++ spec/models/animal_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Animal, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "is valid and has attributes" do
+    spider_monkey = Animal.create common_name: 'Spider Monkey', latin_name: 'spiderus monkeyus', kingdom: 'mountain do'
+    expect(spider_monkey).to be_valid
+  end
+
+  it "is not valid without a common_name" do
+    spider_monkey = Animal.create  latin_name: 'spiderus monkeyus', kingdom: 'mountain do'
+    expect(spider_monkey.errors[:common_name]).to_not be_empty
+  end
+
+  it "is not valid without a latin_name" do
+    spider_monkey = Animal.create  common_name: 'spiderus monkeyus', kingdom: 'mountain do'
+    expect(spider_monkey.errors[:latin_name]).to_not be_empty
+  end
+
+  it "is not valid without a kingdom" do
+    spider_monkey = Animal.create  common_name: 'spiderus monkeyus', latin_name: "present" 
+    expect(spider_monkey.errors[:kingdom]).to_not be_empty
+  end
+  
 end
